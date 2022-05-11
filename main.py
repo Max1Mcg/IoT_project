210812fb891fa535cd21485208f2e11916f3a69f
Max1M <leggo2001@mail.ru>
2022-05-11 14:24:03 +0300

    Updating the project and assembly files according to the changes made

--- main.py
+++ main.py
@@ -21,12 +21,9 @@ payload = {
         'lang': 'ru'
     }
 
-params = ['id', 'name', 'my', 'owner', 'mac', 'cmd', 'location', 'distance', 'time', 'liked', 'uptime',
-'site', 'photo', 'info', 'sensors']
-
 #on/off params
-params_new = {'id':1, 'name':1, 'my':1, 'owner':1, 'mac':1, 'cmd':1, 'location':1, 'distance':1, 'time':1, 'liked':1, 'uptime':1,
-'site':1, 'photo':1, 'info':1, 'sensors':1}
+params_new = {'id':True, 'name':True, 'my':True, 'owner':True, 'mac':True, 'cmd':True,
+'location':True, 'distance':True, 'time':True, 'liked':True, 'uptime':True,'site':True, 'photo':True, 'info':True, 'sensors':True}
 
 #menu
 def menu():
@@ -49,7 +46,8 @@ def menu():
         elif(move == '2'):
             sensors()
         elif(move == '3'):
-            get_values()
+            pass
+            #get_values()
         else:
             print("Действие введено неверно, попробуйте ещё раз")
 
@@ -110,31 +108,8 @@ def sensors():
         else:
             print("Действие введено неверно, попробуйте ещё раз")
 
-#work with [] of sensors
-def get_values():#infinity
-    move = "move"
-    while(True):
-        try:
-            response = requests.post(url=urll, json=payload)
-            print(response)
-            temp = response.json()
-            for i in params:
-                print(temp['devices'][0][i])
-            print('введите end для окончания сбора информации или любой другой символ для продолжения')
-            move = input()
-            if (move == "end"):
-                return
-            print("ждите 30 секунд до следующего получения информации с датчика")
-            time.sleep(10)
-            for i in range(10, 21, 10):
-                print("прошло " + str(i) + " секунд")
-                time.sleep(10)
-        except Exception:
-            print("error!!!")
-            break
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    UIWindow = MWindow(payload, params, urll)
-    app.exec_()
+#if __name__ == "__main__":
+app = QApplication(sys.argv)
+UIWindow = MWindow(payload, params_new, urll)
+app.exec_()
 
